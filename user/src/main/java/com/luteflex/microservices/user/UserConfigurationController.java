04ff4967b7b37ea46ff24377d1f07a7f95d6f2aa
LuteIsCool <l.vanoosten@student.fontys.nl>
2020-05-23 22:08:52 +0200

    RabbitMq opgezet

--- user/src/main/java/com/luteflex/microservices/user/UserConfigurationController.java
+++ user/src/main/java/com/luteflex/microservices/user/UserConfigurationController.java
@@ -1,14 +1,17 @@
 package com.luteflex.microservices.user;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.luteflex.microservices.user.bean.UserConfiguration;
+
+@CrossOrigin(value = "*")
 @RestController
 public class UserConfigurationController
 {
 
 
-    @GetMapping("/lute")
+    @GetMapping("/user")
     public UserConfiguration retriveLimitsFromConfigurations()
     {
         return new UserConfiguration(1000, 1);
