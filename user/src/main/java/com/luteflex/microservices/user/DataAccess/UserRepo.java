10b2399384b9666a3280987e9bdba7ec46089613
LuteIsCool <l.vanoosten@student.fontys.nl>
2020-06-07 19:47:50 +0200

    login foutmelding bij geen account

--- user/src/main/java/com/luteflex/microservices/user/DataAccess/UserRepo.java
+++ user/src/main/java/com/luteflex/microservices/user/DataAccess/UserRepo.java
@@ -2,10 +2,7 @@ package com.luteflex.microservices.user.DataAccess;
 
 import com.luteflex.microservices.user.Models.User;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import javax.persistence.TypedQuery;
+import javax.persistence.*;
 
 public class UserRepo {
     private EntityManager em;
@@ -26,10 +23,16 @@ public class UserRepo {
 
     public User login(User user) {
         createEm();
-        TypedQuery<User> query = em.createQuery(
-                "select u from User u where u.email = :email and u.password = :password", User.class);
-        query.setParameter("email", user.getEmail());
-        query.setParameter("password", user.getPassword());
-        return query.getSingleResult();
+
+        try{
+            TypedQuery<User> query = em.createQuery(
+                    "select u from User u where u.email = :email and u.password = :password", User.class);
+            query.setParameter("email", user.getEmail());
+            query.setParameter("password", user.getPassword());
+            return query.getSingleResult();
+        } catch(NoResultException e) {
+            return null;
+        }
+
     }
 }
